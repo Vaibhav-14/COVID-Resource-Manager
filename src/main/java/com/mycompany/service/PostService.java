57100ecd64c05f8c0405fefffc2a120d9ca18623
sammed_patil <sammed_patil@persistent.com>
2021-07-29 19:22:55 +0530

    Project Structure

--- src/main/java/com/mycompany/service/PostService.java
+++ src/main/java/com/mycompany/service/PostService.java
@@ -1,5 +1,23 @@
 package com.mycompany.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mycompany.dao.IPostFunctionDAO;
+
+@Service("postService")
 public class PostService {
+	
+	@Autowired
+	private IPostFunctionDAO dao;
+
+	public IPostFunctionDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(IPostFunctionDAO dao) {
+		this.dao = dao;
+	}
+	
 
 }
--- src/main/java/com/mycompany/service/UserService.java
+++ src/main/java/com/mycompany/service/UserService.java
@@ -12,7 +12,12 @@ public class UserService {
 	
 	@Autowired
 	private IUserFunctionDAO userDao;
-
+	public IUserFunctionDAO getUserDao() {
+		return userDao;
+	}
+	public void setUserDao(IUserFunctionDAO userDao) {
+		this.userDao = userDao;
+	}
 	public void addUser(User u)
 	{
 		userDao.save(u);
