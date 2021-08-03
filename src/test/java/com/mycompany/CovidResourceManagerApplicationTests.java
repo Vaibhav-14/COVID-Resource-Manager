fa39dd9e801d0f86f754827edbef609f2c4d47df
prasad-wakde <prasad_wakde@persistent.com>
2021-08-03 11:40:33 +0530

    added basic controller and http responses tests

--- src/test/java/com/mycompany/CovidResourceManagerApplicationTests.java
+++ src/test/java/com/mycompany/CovidResourceManagerApplicationTests.java
@@ -1,13 +1,64 @@
+
 package com.mycompany;
 
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.test.web.servlet.MockMvc;
+import com.mycompany.controller.HomeController;
+import com.mycompany.controller.PostController;
+import com.mycompany.controller.UserController;
+import com.mycompany.dao.IPostFunctionDAO;
+import com.mycompany.entity.Post;
+import com.mycompany.entity.User;
+import com.mycompany.service.PostService;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 
 @SpringBootTest
-class CovidResourceManagerApplicationTests {
+@AutoConfigureMockMvc
+public class CovidResourceManagerApplicationTests {
+	
+	@Autowired
+	private PostService service;
+	
+	@MockBean
+	private IPostFunctionDAO repo;
+	
+	@Autowired
+	private PostController postController;
+	@Autowired
+	private HomeController homeController;
+	@Autowired
+	private UserController userController;
+	
+	@Autowired
+	private MockMvc mockMvc;
+
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(postController).isNotNull();
+		assertThat(homeController).isNotNull();
+		assertThat(userController).isNotNull();
 	}
-
+	
+	@Test
+	public void checkHTTPResponse() throws Exception {
+		this.mockMvc.perform(get("/user/login")).andExpect(status().isOk());
+		this.mockMvc.perform(get("/user/register")).andExpect(status().isOk());
+	}
+	
 }
