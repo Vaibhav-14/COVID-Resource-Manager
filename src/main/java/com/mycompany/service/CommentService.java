8c278a0597b3eda7c0e9bb96a715e1d709f546da
Chandrakant Vyavahare <87981385+chandrakant-vy@users.noreply.github.com>
2021-08-14 14:35:17 +0530

    Update CommentService.java

--- src/main/java/com/mycompany/service/CommentService.java
+++ src/main/java/com/mycompany/service/CommentService.java
@@ -65,7 +65,7 @@ public class CommentService {
 		boolean isAdmin = false;
 		Comment comment = commentDao.findById(id).get();
 		User commentUser = comment.getUser();
-		if(commentUser.getId() == user.getId() || (isAdmin = user.getRoles().contains(role)) || comment.getPost().getUser().getId() == user.getId()) {
+		if(commentUser.getId() == user.getId() || comment.getPost().getUser().getId() == user.getId() || (isAdmin = user.getRoles().contains(role))) {
 			commentDao.deleteById(id);
 			if (isAdmin) {
 				logger.warn("Admin has deleted comment of user : " + commentUser.getUsername());
