b12f1d1971209b658d731c5f161d4a42f35aeff3
Vaibhav-14 <87973352+Vaibhav-14@users.noreply.github.com>
2021-08-16 12:55:15 +0530

    Vaccination API done

--- src/main/java/com/mycompany/controller/VaccinationController.java
+++ src/main/java/com/mycompany/controller/VaccinationController.java
@@ -1,5 +1,7 @@
 package com.mycompany.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
@@ -7,6 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.mycompany.entity.CenterDetails;
+import com.mycompany.entity.Session;
 
 @RestController
 public class VaccinationController {
@@ -15,10 +21,14 @@ public class VaccinationController {
 	private RestTemplate restTemplate;
 	
 	@GetMapping("/vaccination")
-	public ResponseEntity<Object> getDetailsByPin(@RequestParam String pincode, @RequestParam String date) {
+	public ModelAndView getDetailsByPin(@RequestParam String pincode) {
+
+		String date= "16-08-2021";
 		String url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode="+ pincode + "&date=" + date;
-		ResponseEntity<Object> vaccines = restTemplate.getForEntity(url, Object.class);
-		return vaccines;
+		List<Session> vaccines = restTemplate.getForEntity(url, CenterDetails.class).getBody().getSessions();
+
+		return new ModelAndView("vaccine","vaccines",vaccines);
+
 	}
 	
 }
